b7778ebb4f5c40ecc1872022e3a74d955b7302c2
VinayAdarshRS <vinay@rightanglesol.com>
2024-05-29 07:13:25 -0400

    FloodCoreApplicationReport
    Added Comma Separator's for Summary of Discharges

--- PresTrustDevExReportsService/Src/APIs/PresTrust.DevExReports.API/PredefinedReports/Flood/FloodCoreApplicationReportGeneral.Designer.cs
+++ PresTrustDevExReportsService/Src/APIs/PresTrust.DevExReports.API/PredefinedReports/Flood/FloodCoreApplicationReportGeneral.Designer.cs
@@ -214,9 +214,9 @@ namespace PresTrust.DevExReports.API.PredefinedReports.Flood {
             this.label11 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label11");
             this.label10 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label10");
             this.label9 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label9");
-            this.pictureBox1 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRPictureBox>("pictureBox1");
             this.label98 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label98");
             this.label8 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label8");
+            this.pictureBox1 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRPictureBox>("pictureBox1");
 
             // Parameters
             this.ApplicationTitle = reportInitializer.GetParameter("ApplicationTitle");
@@ -422,9 +422,9 @@ namespace PresTrust.DevExReports.API.PredefinedReports.Flood {
         private DevExpress.XtraReports.UI.XRLabel label11;
         private DevExpress.XtraReports.UI.XRLabel label10;
         private DevExpress.XtraReports.UI.XRLabel label9;
-        private DevExpress.XtraReports.UI.XRPictureBox pictureBox1;
         private DevExpress.XtraReports.UI.XRLabel label98;
         private DevExpress.XtraReports.UI.XRLabel label8;
+        private DevExpress.XtraReports.UI.XRPictureBox pictureBox1;
         private DevExpress.DataAccess.Sql.SqlDataSource sqlDataSource1;
         private DevExpress.XtraReports.Parameters.Parameter ApplicationTitle;
     }
