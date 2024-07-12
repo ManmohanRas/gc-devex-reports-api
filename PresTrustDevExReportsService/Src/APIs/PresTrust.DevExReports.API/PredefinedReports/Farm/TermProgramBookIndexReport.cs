bb21ab081879f2ed54a04e0e47b5ed7f60694472
narayanaamara7 <155095387+narayanaamara7@users.noreply.github.com>
2024-07-13 00:35:42 +0530

    Farm reports
    Farm reports

---
+++ PresTrustDevExReportsService/Src/APIs/PresTrust.DevExReports.API/PredefinedReports/Farm/TermProgramBookIndexReport.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace PresTrust.DevExReports.API.PredefinedReports.Farm
+{
+    public partial class TermProgramBookIndexReport
+    {
+        public TermProgramBookIndexReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
