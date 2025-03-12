23ca3087f795a7629dbf7761290324a261cd42ef
VinayAdarshRS <vinay@rightanglesol.com>
2025-03-12 07:22:29 -0400

    Exhibit a and Summary of preserved farms report Fixes

--- PresTrustDevExReportsService/Src/APIs/PresTrust.DevExReports.API/PredefinedReports/Farm/FarmEsmtSummaryOfPreservedFarms.Designer.cs
+++ PresTrustDevExReportsService/Src/APIs/PresTrust.DevExReports.API/PredefinedReports/Farm/FarmEsmtSummaryOfPreservedFarms.Designer.cs
@@ -114,8 +114,8 @@ namespace PresTrust.DevExReports.API.PredefinedReports.Farm {
             this.label13 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label13");
             this.label14 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label14");
             this.pictureBox1 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRPictureBox>("pictureBox1");
-            this.label6 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label6");
             this.label1 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label1");
+            this.label6 = reportInitializer.GetControl<DevExpress.XtraReports.UI.XRLabel>("label6");
 
             // Data Sources
             this.sqlDataSource1 = reportInitializer.GetDataSource<DevExpress.DataAccess.Sql.SqlDataSource>("sqlDataSource1");
@@ -219,7 +219,7 @@ namespace PresTrust.DevExReports.API.PredefinedReports.Farm {
         private DevExpress.XtraReports.UI.XRLabel label14;
         private DevExpress.XtraReports.UI.XRPictureBox pictureBox1;
         private DevExpress.DataAccess.Sql.SqlDataSource sqlDataSource1;
-        private DevExpress.XtraReports.UI.XRLabel label6;
         private DevExpress.XtraReports.UI.XRLabel label1;
+        private DevExpress.XtraReports.UI.XRLabel label6;
     }
 }
